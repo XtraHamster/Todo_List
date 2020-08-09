65a366e25fdfcfd3bac89c545b4e22167fe77b62
XtraHamster <martinkrecek98@seznam.cz>
2020-08-09 12:09:58 +0200

    Delete added
    Added delete function. Todo can be deleted now by pressing the X.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $('li').click(function(){
 	$(this).toggleClass('completed');
 });
+
+// Clicks on X to delete Todo
+$('span').click(function(){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
